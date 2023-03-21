4959a2ac2d16a10826447a5e2d2309f42de83c56
Lutz Herrmann <lutz.herrmann@mailbox.org>
2023-03-21 13:57:57 +0100

    commit 001

--- fmt/main.go
+++ fmt/main.go
@@ -23,18 +23,18 @@ func main() {
 	// Output:
 	// 002: Hallo, Lutz
 
-	// Verben
-	// %v Die Ausgabe des Wertes einer beliebigen Variablen. Dabei kann jeder Typ über dieses Verb ausgegeben werden
-	// %+v Gibt bei Strukturen zusätzlich die Namen der einzelnen Felder aus.
-	// %#v Die Ausgabe erfolgt so, wie wir diese als Go-Code definieren würden.
-	// %T Gibt den Typ der Variablen aus.
-	// %b Ausgabe zur Basis 2, also als binäre Zahl. Dieses Verb ist nur für Integer zulässig
-	// %d Ausgabe zu Basis 10
-	// %03d oder %03b: Zahlen werden mit 0 aufgefüllt
-	// %p Gibt die Adresse im Speicher aus, also den Pointer der Variablen
-	// %s Direkte Ausgabe eines String
-	// %x Ausgabe eines String als hexadeezimaler Wert
-	// https://golang.org/pkg/fmt/
+	/* Verben
+	%v Die Ausgabe des Wertes einer beliebigen Variablen. Dabei kann jeder Typ über dieses Verb ausgegeben werden
+	%+v Gibt bei Strukturen zusätzlich die Namen der einzelnen Felder aus.
+	%#v Die Ausgabe erfolgt so, wie wir diese als Go-Code definieren würden.
+	%T Gibt den Typ der Variablen aus.
+	%b Ausgabe zur Basis 2, also als binäre Zahl. Dieses Verb ist nur für Integer zulässig
+	%d Ausgabe zu Basis 10
+	%03d oder %03b: Zahlen werden mit 0 aufgefüllt
+	%p Gibt die Adresse im Speicher aus, also den Pointer der Variablen
+	%s Direkte Ausgabe eines String
+	%x Ausgabe eines String als  hexadezimaler Wert
+	https://golang.org/pkg/fmt/ */
 
 	s1 := fmt.Sprintf("%03d: Hallo, %s\n", nr, name)
 	fmt.Print(s1)
